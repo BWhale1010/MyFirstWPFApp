3564f844f22e7a473596e3d82c744b6990e62d7e
BWhale1010 <bawool1010@daum.net>
2025-01-13 11:19:29 +0900

    wpf MVVM 패턴 실습

--- MyFirstWPFApp/App.xaml.cs
+++ MyFirstWPFApp/App.xaml.cs
@@ -9,6 +9,11 @@ namespace MyFirstWPFApp
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow main = new();
+            main.ShowDialog();
+        }
     }
 
 }
--- MyFirstWPFApp/Views/MainWindow.xaml.cs
+++ MyFirstWPFApp/Views/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MyFirstWPFApp.ViewModels;
 
 namespace MyFirstWPFApp
 {
@@ -19,6 +20,7 @@ namespace MyFirstWPFApp
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
